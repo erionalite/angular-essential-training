0f081c8826fd752878f301c59fbac76c07ae41ae
Erion Alite <erion.alite@gmail.com>
2021-09-12 23:29:05 -0400

    add modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import {AppComponenet} from './app.componenet'
+@NgModule(
+    {
+        imports: [
+            BrowserModule
+        ],
+        declarations:[],
+        bootstrap:[
+            AppComponenet
+        ]
+    }
+)
+export class AppModule{}
